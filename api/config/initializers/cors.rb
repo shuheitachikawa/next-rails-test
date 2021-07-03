d446f509017ffdd61f1675b0b938e217ee517e58
shuhei.tachikawa <shuhei.tusx@gmail.com>
2021-07-04 02:22:02 +0900

    test

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -8,7 +8,7 @@
 # 追加
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['localhost:8000', 'localhost:3000']
+    origins ['localhost:8000', 'localhost:3000', 'https://next-rails-test.vercel.app']
 
     resource '*',
       headers: :any,
